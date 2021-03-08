0ed4e22e00ecfa176e2e9d34e9d689172e8c2872
AlessandroSaviolo <alessandro.saviolo@hotmail.com>
2021-03-08 11:32:18 +0100

    Update track.py

--- src/track.py
+++ src/track.py
@@ -1,5 +1,19 @@
-import numpy as np
+#Copyright (C) 2021 Alessandro Saviolo
 
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
 
 def getCircle():
   return np.array([[0.75, 2.0, -2.0199999809265137],
